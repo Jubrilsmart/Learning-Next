05cf66bc0a367d2e302029516537a182e0430d81
jubrilsmart <jubrilsmart1@gmail.com>
2026-01-04 20:49:11 +0100

    second day learning next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+import './index.css';
 export const metadata = {
     title: 'Home Page',
     description: 'This is the home page of our Next.js application.'
@@ -6,9 +8,22 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html>
             <body>
-                <header style={{ height: '50px', backgroundColor: 'green', margin: '20px'}}>Here is my Header</header>
+                <header className='bg-indigo-600 py-5 text-2xl my-4 flex justify-between px-4'>
+                    Here is my Header
+                    <div>
+                        <Link href='/' className='ml-4 text-white'>Home</Link>
+                        <Link href='/about' className='ml-4 text-white'>About</Link>
+                        <Link href='/contact' className='ml-4 text-white'>Contact</Link>
+                        <Link href='/blog' className='ml-4 text-white'>Blog</Link>
+                    </div>
+                    <div>
+                        <Link href='/login' className='ml-4 text-white'>Login</Link>
+                        <Link href='/register' className='ml-4 text-white'>Register</Link>
+                        <Link href='/sign-up' className='ml-4 text-white'>Sign-up</Link>
+                    </div>
+                </header>
                 {children}
-                <footer style={{ height: '60px', backgroundColor: 'red', margin: '20px' }}>Here is my Footer</footer>
+                <footer className='bg-yellow-600 py-5 text-2xl my-4'>Here is my Footer</footer>
             </body>
         </html>
     );
